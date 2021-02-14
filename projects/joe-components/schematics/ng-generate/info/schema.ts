974b2245876cadd61551ddcf32ccfc5924e5438d
Joe Lin <>
2021-02-14 17:54:54 +0800

    增加註解

--- projects/joe-components/schematics/ng-generate/info/schema.ts
+++ projects/joe-components/schematics/ng-generate/info/schema.ts
@@ -1,8 +1,14 @@
 export interface Schema {
+  /** 專案 */
   project: string;
+  /** 使用者名稱 */
   userName: string;
+  /** 工作經驗 */
   workExperience: number;
+  /** 職位 */
   job: 'Frontend' | 'Backend' | 'Full Stack';
+  /** 喜歡的程式語言 */
   programmingLanguages: string[];
+  /** 是否在職 */
   isEmployed: boolean;
 }
